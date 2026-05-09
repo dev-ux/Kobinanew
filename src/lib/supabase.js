5fd21e65c3567794b086f5fa84e9dc7be27ff631
Junior Marvin <marvinjunior0958@gmail.com>
2026-05-09 04:50:33 +0200

    Fix supabase environment variable name from RREACT_APP_SUPABASE_URL to REACT_APP_SUPABASE_URL

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.RREACT_APP_SUPABASE_URL;
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE;
 
 const isConfigured =
